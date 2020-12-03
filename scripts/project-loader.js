538aab96bf0dc4beb8f53940b4ffc70d98b42925
Inviro <prelusiondilemma@gmail.com>
2020-12-03 05:26:06 -0800

    Started working on implementing a way to display my repositories

--- scripts/project-loader.js
+++ scripts/project-loader.js
@@ -1,7 +1,30 @@
-function runOnLoad() {
-    console.log("Hello World!");
+/**
+   * loadProject takes a childNode and alters it based upon information pulled using scripts
+   * @param {Element} entry The element to be altered
+*/
+function loadProject(entry) {
+    const userName = "Inviro";
+    let title = entry.firstElementChild.innerHTML;
+    let caption = entry.lastElementChild.innerHTML;
+    console.log(title);
+    console.log(caption);
+    // switch(title){
+    //     case "Illud":
+    //         break;
+    //     default:
+    // }
+}
+
+/**
+   * loadProjects runs on window load and edits the HTML dom with the project information for each project
+*/
+function loadProjects() {
+    var projectList = document.getElementsByClassName("project");
+    for(var i = 0; i < projectList.length; i++) {
+        loadProject(projectList[i]);
+    }
 }
 
 window.onload = function() {
-    runOnLoad();
+    loadProjects();
 }
