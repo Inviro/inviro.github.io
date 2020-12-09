1660b8852ab08bbeecb31211989a4aca5e080c0f
Inviro <prelusiondilemma@gmail.com>
2020-12-08 17:54:15 -0800

    Minimized css and redid the logic for project loading

--- scripts/project-loader.js
+++ scripts/project-loader.js
@@ -1,17 +1,28 @@
 /**
-   * loadProject takes a childNode and alters it based upon information pulled using scripts
-   * @param {Element} entry The element to be altered
+   * loadProject takes the ID of a project and alters the project based upon information pulled using scripts
+   * @param {String} entryID The ID of the entry to be altered
 */
-function loadProject(entry) {
-    const userName = "Inviro";
+function loadProject(entryID) {
+    let entry = document.getElementById(entryID);
     let title = entry.firstElementChild.innerHTML;
     let caption = entry.lastElementChild.innerHTML;
+    console.log(title);
+    console.log(caption);
+
+    title = "<a href=" + "https://github.com/inviro/" + title + ">" + title + "</a>";
+    entry.firstElementChild.innerHTML = title;
+    // console.log(temp);
+    // const userName = "Inviro";
+    // let title = entry.firstElementChild.innerHTML;
+    // let caption = entry.lastElementChild.innerHTML;
     // title = "<a href=" + "https://github.com/inviro/" + title + ">" + title + "</a>";
     // console.log("<a href=" + "https://github.com/inviro/" + title + ">" + title + "</a>");
     // entry.firstChild.innerHTML = "HELLO WORLD";
-    console.log(title);
-    console.log(caption);
-    console.log(entry);
+
+    // console.log(title);
+    // console.log(caption);
+    // console.log(entry);
+
     // switch(title){
     //     case "Illud":
     //         break;
@@ -23,14 +34,14 @@ function loadProject(entry) {
    * loadProjects runs on window load and edits the HTML dom with the project information for each project
 */
 function loadProjects() {
-    let projectList = document.getElementsByClassName("project");
-    for(var i = 0; i < projectList.length; i++) {
-        loadProject(projectList.item(i));
+
+    let projectListLength = document.getElementsByClassName("project").length;
+    // console.log(projectListLength);
+    for(var i = 0; i < projectListLength; i++) {
+        loadProject("project-" + (i + 1));
     }
-    console.log(projectList);
 }
 
 window.onload = function() {
     loadProjects();
-    // console.log("Hello World");
 }
