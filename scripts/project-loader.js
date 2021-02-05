b4fa264387bc158ce5d15861120f529fda5c3071
Inviro <prelusiondilemma@gmail.com>
2021-02-05 02:19:47 -0800

    Added linter to enforce code style

--- scripts/project-loader.js
+++ scripts/project-loader.js
@@ -1,154 +1,158 @@
 /**
-   * setRepoStats takes the ID of a project and the repository name and updates its information accordingly
+   * setRepoStats takes the ID of a project and the repository name and updates its information
    * @param {String} repoName The name of the repository to search for
    * @param {Number} projectID The ID of the project to be altered
 */
-async function setRepoStats(repoName, projectID) {
-    const REPO_URL = "https://api.github.com/repos/inviro/" + repoName; // Repository location in Github API
-    // Using JavaScript fetch API to perform Github API calls for repository information
-    fetch(REPO_URL, {
-        method: 'GET', // GET request
-        headers:{
-            "Accept"       : "application/json",    // Accept JSON Files
-            "Content-Type" : "application/json",    // JSON content
-            "User-Agent"   : "AbrahamChen.me"       // Website user agent
-        }
-    }).then(response => response.json()).then(data => { // Anonomous functions that convert the response into JSON format
-        // Updating the repository information from the information obtained from the GET request
-        document.getElementById("watch-" + projectID).innerHTML = " " + data.watchers_count;
-        document.getElementById("star-" + projectID).innerHTML = " " + data.stargazers_count;
-        document.getElementById("fork-" + projectID).innerHTML = " " + data.forks_count;
-        document.getElementById("caption-" + projectID).innerHTML = " " + data.description;
-    })
+function setRepoStats(repoName, projectID) {
+  const REPO_URL = `https://api.github.com/repos/inviro/${repoName}`; // Repository location in Github API
+  // Using JavaScript fetch API to perform Github API calls for repository information
+  fetch(REPO_URL, {
+    method: 'GET', // GET request
+    headers: {
+      Accept: 'application/json', // Accept JSON Files
+      'Content-Type': 'application/json', // JSON content
+      'User-Agent': 'AbrahamChen.me', // Website user agent
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => { // Anonomous functions that convert the response into JSON format
+      // Updating the repository information from the information obtained from the GET request
+      document.getElementById(`watch-${projectID}`).innerHTML = ` ${data.watchers_count}`;
+      document.getElementById(`star-${projectID}`).innerHTML = ` ${data.stargazers_count}`;
+      document.getElementById(`fork-${projectID}`).innerHTML = ` ${data.forks_count}`;
+      document.getElementById(`caption-${projectID}`).innerHTML = ` ${data.description}`;
+    });
 }
 
 /**
-   * loadProject takes the ID of a project and alters the project based upon information pulled using scripts
+   * loadProject takes the ID of a project and alters the project based upon pulled information
    * @param {String} entryID The ID of the entry to be altered
 */
-async function loadProject(entryID) {
-    let entry = document.getElementById(entryID);
-    let title = entry.firstElementChild.innerHTML;
+function loadProject(entryID) {
+  const entry = document.getElementById(entryID);
+  const title = entry.firstElementChild.innerHTML;
 
-    let result;
-    const PROJECT_ID = entryID.split("-")[1]; // Gets project ID by getting the second split element
+  let result;
+  const PROJECT_ID = entryID.split('-')[1]; // Gets project ID by getting the second split element
 
-    // Setting title to a github link
-    result = "<a href=" + "https://github.com/inviro/" + title + ">" + title + "</a>";
-    entry.firstElementChild.innerHTML = result;
+  // Setting title to a github link
+  // result = `${'<a href=' + 'https://github.com/inviro/'}${title}>${title}</a>`;
+  entry.firstElementChild.innerHTML = result;
 
-    // Adding icons for the github stats that can be be updated dynamically with numbers later on
-    // Uses FontAwesome for the icons
-    result = "<a href=\"https://github.com/inviro/" + title + "/watchers\" aria-label = \"" + title + " Watchers\"><i class=\"fa fa-lg fa-eye\" id=\"watch-" + PROJECT_ID +"\"></i></a>";
-    result += "<a href=\"https://github.com/inviro/" + title + "/stargazers\" aria-label = \"" + title + " Stargazers\"><i class=\"fa fa-lg fa-star\" id=\"star-" + PROJECT_ID +"\"></i></a>";
-    result += "<a href=\"https://github.com/inviro/" + title + "/network/members\" aria-label = \"" + title + " Forks\"><i class=\"fa fa-lg fa-code-branch\" id=\"fork-" + PROJECT_ID + "\"></i></a>";
-    document.getElementById("repo-stats-" + PROJECT_ID).innerHTML = result;
-    setRepoStats(title, PROJECT_ID);
+  // Adding icons for the github stats that can be be updated dynamically with numbers later on
+  // Uses FontAwesome for the icons
+  result = `<a href="https://github.com/inviro/${title}/watchers" aria-label = "${title} Watchers"><i class="fa fa-lg fa-eye" id="watch-${PROJECT_ID}"></i></a>`;
+  result += `<a href="https://github.com/inviro/${title}/stargazers" aria-label = "${title} Stargazers"><i class="fa fa-lg fa-star" id="star-${PROJECT_ID}"></i></a>`;
+  result += `<a href="https://github.com/inviro/${title}/network/members" aria-label = "${title} Forks"><i class="fa fa-lg fa-code-branch" id="fork-${PROJECT_ID}"></i></a>`;
+  document.getElementById(`repo-stats-${PROJECT_ID}`).innerHTML = result;
+  setRepoStats(title, PROJECT_ID);
 }
 
 /**
-   * loadProjects runs on window load and edits the HTML dom with the project information for each project
+   * loadProjects runs on window load and updates the HTML dom with project information
 */
-async function loadProjects() {
-    const projectListLength = document.getElementsByClassName("project").length;
-    for(let i = 0; i < projectListLength; i++) {
-        loadProject("project-" + i);
-    }
+function loadProjects() {
+  document.getElementsByClassName('project').array.forEach((project) => {
+    loadProject(project);
+  });
 }
 
 /**
    * setThemeLight sets the current theme to light theme
 */
-async function setThemeLight() {
-    let root = document.documentElement;
-    // Setting root css variables to change color theme
-    root.style.setProperty("--primary-color", "3, 4, 94")
-    root.style.setProperty("--overlay-color", "rgba(245, 250, 255, 0.95)")
-    root.style.setProperty("--header-color", "33, 158, 188")
-    root.style.setProperty("--highlight-color", "rgba(255, 255, 255, .90)")
+function setThemeLight() {
+  const root = document.documentElement;
+  // Setting root css variables to change color theme
+  root.style.setProperty('--primary-color', '3, 4, 94');
+  root.style.setProperty('--overlay-color', 'rgba(245, 250, 255, 0.95)');
+  root.style.setProperty('--header-color', '33, 158, 188');
+  root.style.setProperty('--highlight-color', 'rgba(255, 255, 255, .90)');
 }
 
 /**
    * setThemeDark sets the current theme to dark theme
 */
-async function setThemeDark() {
-    let root = document.documentElement;
-    // Setting root css variables to change color theme
-    root.style.setProperty("--primary-color", "255, 255, 255")
-    root.style.setProperty("--overlay-color", "rgba(50, 50, 140, .5)")
-    root.style.setProperty("--header-color", "3, 4, 94")
-    root.style.setProperty("--highlight-color", "#219EBC")
+function setThemeDark() {
+  const root = document.documentElement;
+  // Setting root css variables to change color theme
+  root.style.setProperty('--primary-color', '255, 255, 255');
+  root.style.setProperty('--overlay-color', 'rgba(50, 50, 140, .5)');
+  root.style.setProperty('--header-color', '3, 4, 94');
+  root.style.setProperty('--highlight-color', '#219EBC');
 }
 
 /**
-   * addThemePicker displays the theme picker, and attaches listeners to the corresponding radio buttons
+   * addThemePicker displays the theme picker raido buttons, and attaches listeners to them
 */
-async function addThemePicker() {
-    document.getElementById('color-theme').style.display = 'inline'; // Changes display state from none to inline
-    // Adds listeners to radio buttons to set css variables onclick
-    document.getElementById('theme-light').onclick = setThemeLight;
-    document.getElementById('theme-dark').onclick = setThemeDark;
+function addThemePicker() {
+  document.getElementById('color-theme').style.display = 'inline'; // Changes display state from none to inline
+  // Adds listeners to radio buttons to set css variables onclick
+  document.getElementById('theme-light').onclick = setThemeLight;
+  document.getElementById('theme-dark').onclick = setThemeDark;
 }
 
 /**
    * addAnimations adds animations using JavaScript and css to different inputs
 */
-async function addAnimations() {
-    // Adding animation to logo on mouseover and mouseout
-    let logo = document.getElementsByClassName('logo')[0]; // Gets logo by class name
-    logo.addEventListener("mouseover", () => {
-        logo.style.animation = "rotation 2s infinite linear"; // Adds infinate rotation keyframe on mouseover
-    })
-    logo.addEventListener("mouseout", () => {
-        logo.style.animation = "none"; // Removes rotation keyframe on mouseout
-    })
+function addAnimations() {
+  // Adding animation to logo on mouseover and mouseout
+  const logo = document.getElementsByClassName('logo')[0]; // Gets logo by class name
+  logo.addEventListener('mouseover', () => {
+    logo.style.animation = 'rotation 2s infinite linear'; // Adds infinate rotation keyframe on mouseover
+  });
+  logo.addEventListener('mouseout', () => {
+    logo.style.animation = 'none'; // Removes rotation keyframe on mouseout
+  });
 }
 
 /**
    * addTextAnimation adds animations to text using JavaScript
-   * @param {Element} ele The element which to animate
+   * @param {HTMLElement} ele The element which to animate
    * @param {String} text A string seperated by " | " of text to animate over
 */
-async function addTextAnimation(ele, text) {
-    if(text.indexOf(" | ") > -1) { // If | exists in the element's innerHTML
-        let isPaused = false; // Pauses animation on hover
-        const wordArray = text.split(" | "); // Splits html from a single line into several elements
-        let currIdx = 0; // Keeps track of the current index in wordArray
-        setInterval(() => { // Performs this on an interval
-            if(!isPaused) { // If isPaused is false, do the rest
-                ele.innerHTML = wordArray[currIdx]; // Sets element HTML to the currentIndex
-                currIdx = ((currIdx + 1) % wordArray.length); // Increments index, and ensures that it is within array bounds
-            }
-        }, 4500); // Repeat every x miliseconds
+function addTextAnimation(ele, text) {
+  if (text.indexOf(' | ') > -1) { // If | exists in the element's innerHTML
+    let isPaused = false; // Pauses animation on hover
+    const wordArray = text.split(' | '); // Splits html from a single line into several elements
+    let currIdx = 0; // Keeps track of the current index in wordArray
+    setInterval(() => { // Performs this on an interval
+      if (!isPaused) { // If isPaused is false, do the rest
+        // eslint-disable-next-line no-param-reassign
+        ele.innerHTML = wordArray[currIdx]; // Sets element HTML to the currentIndex
+        // Increments index, and ensures that it is within array bounds
+        currIdx = ((currIdx + 1) % wordArray.length);
+      }
+    }, 4500); // Repeat every x miliseconds
 
-        // Adds pause on mouseover and resumes on mouseout
-        ele.addEventListener("mouseover", () => {
-            isPaused = true;
-        })
-        ele.addEventListener("mouseout", () => {
-            isPaused = false;
-        })
-    }
+    // Adds pause on mouseover and resumes on mouseout
+    ele.addEventListener('mouseover', () => {
+      isPaused = true;
+    });
+    ele.addEventListener('mouseout', () => {
+      isPaused = false;
+    });
+  }
 }
 
 /**
    * updateHitCount updates the count of weekly and total visits to my website
 */
-async function updateHitCount() {
-    const a = "aHR0cHM6Ly91cy1jZW50cmFsMS1hYnJhaGFtY2hlbndlYi5jbG91ZGZ1bmN0aW9ucy5uZXQvd2Vic2l0ZUhpdHMg";
-    let visitsDisplay = document.getElementById('visits-display');
-    await fetch(atob(a))
-        .then((resp) => resp.json()).then((jsonResp) => {
-            var htmlString = `<strong>Visits this week:  </strong><p>${parseInt(jsonResp["weeklyHits"]).toLocaleString()}</p>`;
-            htmlString += ` | <strong>Total visits:  </strong><p>${parseInt(jsonResp["totalHits"]).toLocaleString()}</p>`;
-            addTextAnimation(visitsDisplay, htmlString);
+function updateHitCount() {
+  const a = 'aHR0cHM6Ly91cy1jZW50cmFsMS1hYnJhaGFtY2hlbndlYi5jbG91ZGZ1bmN0aW9ucy5uZXQvd2Vic2l0ZUhpdHMg';
+  const visitsDisplay = document.getElementById('visits-display');
+  fetch(atob(a))
+    .then((resp) => resp.json())
+    .then((jsonResp) => {
+      let htmlString = `<strong>Visits this week:  </strong><p>${parseInt(jsonResp.weeklyHits, 10).toLocaleString()}</p>`;
+      htmlString += ` | <strong>Total visits:  </strong><p>${parseInt(jsonResp.totalHits, 10).toLocaleString()}</p>`;
+      addTextAnimation(visitsDisplay, htmlString);
     });
 }
 
 // Runs on window load
 window.onload = () => {
-    addThemePicker();
-    updateHitCount();
-    addAnimations();
-    loadProjects();
-}
+  addThemePicker();
+  updateHitCount();
+  addAnimations();
+  loadProjects();
+};
